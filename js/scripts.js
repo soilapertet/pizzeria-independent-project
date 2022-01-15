4604c90511bdabc42c124b3dace90504e8ea4dcb
Soila Pertet <nicolepertet@gmail.com>
2022-01-15 12:43:37 +0300

    Create a functtion to collect the price of the pizza based on the size

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 // Business logic
+
+// Create Pizza constructor
 function Pizza(type,size,crust,toppings)
 {
   this.type = type;
@@ -6,7 +8,18 @@ function Pizza(type,size,crust,toppings)
   this.crust = crust;
   this.topppings = toppings;
 }
+
+// Create a function to collect the price of the pizza based on the size
+// Note uses of change() and children() functions
+var getSizeCost = function (){
+  $("select#pizza-size").change(function() {
+    window.sizeCost = parseInt($(this).children("option:selected").val());
+  });
+}
+
+
 // User-interface logic
+
 $(document).ready(function(){
   // Redirect user to another page after hitting "ORDER NOW!"
   $("#order-button").click(function(){
