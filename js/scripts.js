913f0c5884d44645d127085bf80bf4eb3dc31009
Soila Pertet <nicolepertet@gmail.com>
2022-01-15 12:01:39 +0300

    Create a constructor function, Pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+// Business logic
+function Pizza(type,size,crust,toppings)
+{
+  this.type = type;
+  this.size = size;
+  this.crust = crust;
+  this.topppings = toppings;
+}
+// User-interface logic
 $(document).ready(function(){
   // Redirect user to another page after hitting "ORDER NOW!"
   $("#order-button").click(function(){
